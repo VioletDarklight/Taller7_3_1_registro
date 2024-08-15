4167e98c556549079703573e5727cba413ed3135
karenfigueroa <azucarbella7@gmail.com>
2024-08-15 17:44:20 -0300

    arreglo

--- js/validacion.js
+++ js/validacion.js
@@ -1,24 +1,8 @@
-document
-  .getElementById("registrationForm")
-  .addEventListener("submit", function (event) {
-    event.preventDefault(); // Prevenir que se envíe el formulario sin realizar la validación
-
-    // Elementos definidos
-    let password = document.getElementById("password1").value;
-
-
-    // Validación del campo contraseña y confirmar contraseña
-    if (!password || !confirmPassword) {
-      showAlertError("La contraseña debe ser ingresada.");
-      return;
-    }
-
-    if (password.length < 6) {
-      showAlertError("La contraseña debe tener al menos 6 caracteres.");
-      return;
-    }
-
-
-function showAlertError(message) {
-  alert(message);
+function showAlertSuccess() {
+  document.getElementById("alert-success").classList.add("show");
 }
+
+function showAlertError() {
+  document.getElementById("alert-danger").classList.add("show");
+}
+
