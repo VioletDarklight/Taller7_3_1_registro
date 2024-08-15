d00a41aadd7a8303efe33dc38971910e5db81746
LeticiaRon <rongallardoleticia@gmail.com>
2024-08-15 14:50:33 -0300

    Agrego form al index con id 'registrationForm'

--- js/validacion.js
+++ js/validacion.js
@@ -1,7 +1,24 @@
-function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
-}
+document
+  .getElementById("registrationForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevenir que se envíe el formulario sin realizar la validación
 
-function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
+    // Elementos definidos
+    let password = document.getElementById("password1").value;
+
+
+    // Validación del campo contraseña y confirmar contraseña
+    if (!password || !confirmPassword) {
+      showAlertError("La contraseña debe ser ingresada.");
+      return;
+    }
+
+    if (password.length < 6) {
+      showAlertError("La contraseña debe tener al menos 6 caracteres.");
+      return;
+    }
+
+
+function showAlertError(message) {
+  alert(message);
 }
