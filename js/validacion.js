362d289b4fef13be2ce861ae1fc33bbcb30cac63
LeticiaRon <rongallardoleticia@gmail.com>
2024-08-15 21:20:23 -0300

    Agregamos función de contraseñas mayores a 6 caracteres, y agregamos sugerencia de edición para contraseñas no coincidentes

--- js/validacion.js
+++ js/validacion.js
@@ -1,3 +1,13 @@
+//let repetir = getElementById("password2");
+//let contraseña = document.getElementById("password1");
+
+//contraseña  y repetir contraseñas deben coincidir//
+
+//if (contraseña !== repetir) {
+// return showAlertError();
+//}
+
+//Funciones de alertas
 function showAlertSuccess() {
   document.getElementById("alert-success").classList.add("show");
 }
@@ -6,11 +16,34 @@ function showAlertError() {
   document.getElementById("alert-danger").classList.add("show");
 }
 
-//contraseña  y repetir contraseñas deben coincidir//
+// Contraseña de 6 caracteres o más
+let form = document.getElementById("registrationForm");
+let password = document.getElementById("password1");
+let errorAlert = document.getElementById("alert-danger"); //A CHEQUEAR
+let errorMessage = errorAlert.querySelector("p"); //A CHEQUEAR
+let checkPassword = document.getElementById("password2");
 
-let contraseña = getElementById("password1");
-let repetir = getElementById("password2");
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
 
-if (contraseña !== repetir) {
-  return showAlertError()
-}
+  let passwordValue = password.value;
+
+  document.getElementById("alert-success").classList.remove("show");
+  errorAlert.classList.remove("show");
+
+  if (passwordValue.length < 6) {
+    errorMessage.textContent =
+      "La contraseña debe contener al menos 6 caracteres.";
+    showAlertError();
+    return;
+  }
+
+  //Sugerencia de Leti y Flor para las contraseñas que no coinciden
+  //  if (password.value !== checkPassword.value) {
+  //    errorMessage.textContent = "Las contraseñas no coinciden";
+  //    showAlertError();
+  //    return;
+  //  }
+  showAlertSuccess();
+  form.submit();
+});
