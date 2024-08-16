56d7081bf173b6a8fc23692923a0abc96792865b
karenfigueroa <azucarbella7@gmail.com>
2024-08-15 23:29:39 -0300

    modificaciones por error

--- js/validacion.js
+++ js/validacion.js
@@ -1,8 +1,41 @@
-//contraseña  y repetir contraseñas deben coincidir//
 
-let contraseña = getElementById("password1");
-let repetir = getElementById("password2");
-
-if (contraseña !== repetir) {
-  return showAlertError()
+//Funciones de alertas
+function showAlertSuccess() {
+  document.getElementById("alert-success").classList.add("show");
 }
+
+function showAlertError() {
+  document.getElementById("alert-danger").classList.add("show");
+}
+
+// Contraseña de 6 caracteres o más
+let form = document.getElementById("registrationForm");
+let password = document.getElementById("password1");
+let errorAlert = document.getElementById("alert-danger"); 
+let errorMessage = errorAlert.querySelector("p"); 
+let checkPassword = document.getElementById("password2");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  let passwordValue = password.value;
+
+  document.getElementById("alert-success").classList.remove("show");
+  errorAlert.classList.remove("show");
+
+  if (passwordValue.length < 6) {
+    errorMessage.textContent =
+      "La contraseña debe contener al menos 6 caracteres.";
+    showAlertError();
+    return;
+  }
+
+  // contraseñas que no coinciden
+    if (password.value !== checkPassword.value) {
+      errorMessage.textContent;
+      showAlertError();
+     return;
+   }
+  showAlertSuccess();
+  form.submit();
+});
